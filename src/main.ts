0cc2f4ca83bccab46738d61a276e7a1164fdf5d4
Cristobal Salazar <chris.salza@gmail.com>
2020-09-14 14:53:08 -0400

    added trade

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { getDb, closeConnection } from "./db/mongo";
 import api from "./kraken/api";
+import { LINKXBT } from "./util/asset-pairs";
 import _ from "lodash";
 import { Db } from "mongodb";
 import { TICKER_POLL_INTERVAL, WATCHLIST } from "./config";
@@ -8,6 +9,16 @@ async function main() {
   try {
     const db = await getDb();
     startPolling(db);
+    const res = await api.getTradableAssetPairs({ pair: LINKXBT });
+    console.log(res);
+
+    const orderRes = await api.addStandardOrder({
+      pair: LINKXBT,
+      type: "sell",
+      volume: "1",
+      ordertype: "market",
+    });
+    console.log(orderRes);
   } catch (err) {
     console.error(err);
   }
@@ -22,7 +33,7 @@ function startPolling(database: Db) {
 }
 
 async function pollTickerInfo(db: Db) {
-  const results = await api.public.getTickerInfo(WATCHLIST);
+  const results = await api.getTickerInfo(WATCHLIST);
   db.collection("ticker_info").insertMany(
     results.map((i) => {
       i.timestamp = new Date();
