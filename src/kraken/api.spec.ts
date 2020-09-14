d05c03dff20e24c726ad24bd2bbb866062e15c1c
Cristobal Salazar <chris.salza@gmail.com>
2020-09-14 14:53:23 -0400

    removed public from api

--- src/kraken/api.spec.ts
+++ src/kraken/api.spec.ts
@@ -1,13 +1,13 @@
 import api from "./api";
 describe("Testing public api", () => {
   it("Should get the server time", async () => {
-    const res = await api.public.getServerTime();
+    const res = await api.getServerTime();
     expect(res.unixtime).toBeTruthy();
     expect(res.rfc1123).toBeTruthy();
   });
 
   it("Should get asset info", async () => {
-    const res = await api.public.getAssetInfo({});
+    const res = await api.getAssetInfo({});
     console.log(res);
     expect(res).toBeTruthy();
   });
