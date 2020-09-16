718788d5c360c86128bbaf0faa9947aa78f0036d
Cristobal Salazar <chris.salza@gmail.com>
2020-09-16 17:21:18 -0400

    add private test

---
+++ src/api/private-api.spec.ts
@@ -0,0 +1,13 @@
+import KrakenApi from ".";
+import { API_KEY, API_SECRET } from "../config";
+
+describe("Private Api", () => {
+  const api = new KrakenApi(API_KEY, API_SECRET);
+
+  it("Should get account balance", async () => {
+    const res = await api.getAccountBalance();
+    for (const key in res) {
+      expect(res[key]).toBeDefined();
+    }
+  });
+});
